e36d4a3dca971afae67fd0f2a30061dea3762bf1
MGousseff <matthieu.gousseff@cnrs.fr>
2026-05-07 11:40:48 +0200

    test_importQualVar fixed

--- inst/tinytest/test_importQualVar.R
+++ inst/tinytest/test_importQualVar.R
@@ -31,23 +31,23 @@
 
 expect_silent(
   utrfRedonBDT<-
-    importQualVar(dirPath=paste0(system.file("extdata", package = "lczexplore"),"/lczfiles/Redon"),
-                 file="rsu_utrf_area.geojson", column="TYPO_MAJ", geomID="ID_RSU", confid="UNIQUENESS_VALUE")
+    importQualVar(dirPath=paste0(system.file("extdata", package = "lczexplore"),"/utrfFiles"),
+                 file="bdt_utrf_area.fgb", column="TYPO_MAJ", geomID="ID_RSU", confid="UNIQUENESS_VALUE")
 )
 
 # map2<-showLCZ(utrfRedonBDT,column = "TYPO_MAJ",repr="alter")
 
 expect_silent(
   utrfRedonOSM<-
-    importQualVar(dirPath=paste0(system.file("extdata", package = "lczexplore"),"/osm/2022/Redon"),
-                  file="rsu_utrf_area.geojson", column="TYPO_MAJ",geomID="ID_RSU",confid="UNIQUENESS_VALUE")
+    importQualVar(dirPath=paste0(system.file("extdata", package = "lczexplore"),"/utrfFiles"),
+                  file="osm_utrf_area.fgb", column="TYPO_MAJ",geomID="ID_RSU",confid="UNIQUENESS_VALUE")
 )
 
 #summary(st_geometry_type(utrfRedonOSM))
 utrfComparison<-
   compareLCZ(sf1=utrfRedonBDT, column1="TYPO_MAJ", wf1=" UTRF BDT",
              sf2=utrfRedonOSM, column2="TYPO_MAJ", wf2 = " UTRF OSM", 
-           location = " Redon",exwrite=FALSE,repr="alter", saveG="")
+           location = " Redon",exwrite=FALSE,repr="alter", saveG="", plotNow = FALSE)
 
 # utrfComparison$matConfPlot %>% print
 # utrfComparison$data
